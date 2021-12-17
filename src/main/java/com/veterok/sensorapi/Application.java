b0d5aef1fc75864092cac4df00aba8e457002dd2
Viktor-S <Eg4APKnG43>
2021-12-17 22:22:34 +0200

    added swagger

--- src/main/java/com/veterok/sensorapi/Application.java
+++ src/main/java/com/veterok/sensorapi/Application.java
@@ -1,5 +1,7 @@
 package com.veterok.sensorapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
@@ -8,6 +10,7 @@ import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRep
 @SpringBootApplication
 @EnableReactiveMongoRepositories
 @EnableReactiveMongoAuditing
+@OpenAPIDefinition(info = @Info(title = "Sensor Api", version = "1.0", description = "Documentation APIs v1.0"))
 public class Application {
 
     public static void main(String[] args) {
